0e12876411bf31078c48cdcade370230916d9709
sabrina tsui <tsuisabrina@gmail.com>
2020-03-06 16:34:40 -0800

    add readme and parse ids better

---
+++ README.md
@@ -0,0 +1,16 @@
+Server configuration
+Server1: kafka producer and cluster
+Server2: spark application
+Server3: moongen packet replay
+
+To start Kafka producer
+Start zookeeper:
+bin/zookeeper-server-start.sh config/zookeeper.properties
+Start kafka cluster:
+bin/kafka-server-start.sh config/server.properties
+Create topic named 'test':
+bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic test
+
+To run the spark application
+cd into the spark directory from server 2
+./spark-2.4.5-bin-hadoop2.7/bin/spark-submit --class "Main" netai_project.jar 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,7 @@ import org.apache.spark.SparkContext;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.Optional;
+import org.apache.spark.api.java.function.Function;
 import org.apache.spark.input.StreamInputFormat;
 import org.apache.spark.rdd.RDD;
 import org.apache.spark.streaming.Durations;
@@ -22,7 +23,6 @@ import org.apache.spark.streaming.kafka.*;
 import scala.Tuple2;
 
 import java.util.*;
-import java.util.function.Function;
 
 import org.apache.spark.streaming.api.java.*;
 
@@ -32,13 +32,42 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 
         SparkConf sparkConf = new SparkConf().setMaster("local[*]").setAppName("KafkaConsumer");
-        String packet_topics = "test";
-        String brokers = "localhost:9092";
+        String packet_topics = "TutorialTopic";
+        String brokers = "128.111.52.91:9092";
         JavaStreamingContext streamingContext = new JavaStreamingContext(
                 sparkConf, Durations.seconds(1));
         Set<String> topicsSet = new HashSet<>(Arrays.asList(packet_topics.split(",")));
         Map<String, String> kafkaParams = new HashMap<>();
         kafkaParams.put("metadata.broker.list", brokers);
+
+        //JavaRDD<String> IDSDataSet = streamingContext.sparkContext().textFile("file:///home/user/spark/IDSLogs/alert_csv.txt", 1);
+        JavaRDD<String> IDSDataSet = streamingContext.sparkContext().textFile("file:///home/sabrina/IdeaProjects/netai_project/IDSLogs/1.txt", 1);
+
+
+        JavaPairRDD<String, String> IDSKeyValuePair = IDSDataSet.mapToPair(k -> {
+            String[] a = k.split(",");
+            if (!a[0].isEmpty() && !a[1].isEmpty() && !a[2].isEmpty() && !a[3].isEmpty() && !a[4].isEmpty()){
+                String key = String.join(",",a[0],a[1],a[2],a[3],a[4]).replaceAll("\\s","");
+                String value = String.join(",",a[5],a[6],a[7]);
+                return new Tuple2<>(key, value);
+            }
+            return null;
+        });
+
+        //remove null items from IDSKeyValuePair
+        IDSKeyValuePair = IDSKeyValuePair.filter(new Function<Tuple2<String, String>, Boolean>() {
+            @Override
+            public Boolean call(Tuple2<String, String> v1) throws Exception {
+                return v1 != null;
+            }
+        });
+        IDSKeyValuePair = IDSKeyValuePair.distinct();
+
+        IDSKeyValuePair.foreach(data -> {
+            if (data != null) {
+                System.out.println("key=" + data._1() + " value=" + data._2());
+            }
+        });
 
         JavaPairInputDStream<String, String> directKafkaStream =
                 KafkaUtils.createDirectStream(
@@ -54,27 +83,15 @@ public class Main {
         streamingContext.sparkContext().setLogLevel("ERROR");
 //        //directKafkaStream.print();
 
+        System.out.println("Number of KV pairs : " + IDSKeyValuePair.count());
 
-        JavaRDD<String> IDSDataSet = streamingContext.sparkContext().textFile("file:///home/swaroop/Kafka_Tar/MyProject/netai_project/IDSLogs/1.txt", 1);
-
-        JavaPairRDD<String, String> IDSKeyValuePair = IDSDataSet.mapToPair(k -> {
-            String[] a = k.split(",");
-            return new Tuple2<>(a[0] + "," + a[1] + "," + a[2] + "," + a[3] + "," + a[4], a[5]);
-        });
-
-//        IDSKeyValuePair.foreach(data -> {
-//            System.out.println("key="+data._1() + " value=" + data._2());
-//        });
-
+        JavaPairRDD<String, String> finalIDSKeyValuePair = IDSKeyValuePair;
         directKafkaStream.foreachRDD(rdd->{
-            JavaPairRDD<String, Tuple2<String, Optional<String>>> out = rdd.leftOuterJoin(IDSKeyValuePair);
+            JavaPairRDD<String, Tuple2<String, Optional<String>>> out = rdd.leftOuterJoin(finalIDSKeyValuePair);
             out.foreach(data -> {
                 System.out.println("final_key="+data._1() + " final_value=" + data._2());
             });
         });
-
-
-
 
         //directKafkaStream.print();
 
